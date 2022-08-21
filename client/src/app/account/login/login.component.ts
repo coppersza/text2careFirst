6ae4d00d82f1ebe15075fe623fad093946eefb80
coppersza <89531543+coppersza@users.noreply.github.com>
2022-08-21 15:24:22 +0200

    End of section 17

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  returnUrl: string;
+
+  constructor(private accountService: AccountService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/shop';
+    this.createLoginForm();
+  }
+
+  createLoginForm(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
+      password: new FormControl('', Validators.required)
+    })
+  }
+  onSubmit(){
+    console.log(this.loginForm.value);
+    this.accountService.login(this.loginForm.value).subscribe(() => {
+      this.router.navigateByUrl(this.returnUrl);
+      console.log('user logged in');
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,20 +7,25 @@ import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component'
 import { RouterModule } from '@angular/router';
-
+import { TextInputComponent } from './components/text-input/text-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
     OrderTotalsComponent,
-    BasketSummaryComponent
+    BasketSummaryComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
-    RouterModule
+    BsDropdownModule.forRoot(),
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     PaginationModule,
@@ -28,7 +33,10 @@ import { RouterModule } from '@angular/router';
     PagerComponent,
     CarouselModule,
     OrderTotalsComponent,
-    BasketSummaryComponent
+    BasketSummaryComponent,
+    ReactiveFormsModule,
+    BsDropdownModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
